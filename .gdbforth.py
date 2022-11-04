6b564efcb55ce230cb6758859ce00a3deb88a22b
Nick Pascucci <ndpi@alum.mit.edu>
2022-11-04 20:25:08 +0000

    fix: make dict print logic more robust

--- .gdbforth.py
+++ .gdbforth.py
@@ -10,15 +10,10 @@ F_IMMED = 0x80
 F_HIDDEN = 0x20
 F_LENMASK = 0x1f
 
-def align(addr, count):
-    """Adjust the given address so it is aligned to the given number of bytes."""
-    miss = addr % count
-    return addr - miss
-
-
-def resolve_symbol(addr):
-    """Resolve the name of the symbol at ADDR"""
-    return gdb.execute(f"info symbol 0x{addr:x}", False, True).strip()
+def align(addr):
+    """Adjust the given address so it is aligned to a 4-byte boundary."""
+    aligned_addr = addr.cast(ARCHITECTURE.integer_type(32, False)) + 3 & -3
+    return aligned_addr
 
 
 class DictEntryDumpCmd(gdb.Command):
@@ -29,9 +24,9 @@ class DictEntryDumpCmd(gdb.Command):
             "lookup", gdb.COMMAND_USER
         )
 
-    def _cfa_to_dfa(self, cfa_addr):
-        """Convert a code field address to a dictionary field address."""
-        addr = cfa_addr
+    def _to_dfa(self, addr):
+        """Find the dictionary field address for a word."""
+        addr = align(addr)
         for offset in range(64):
             # null padding should be address aligned so we will get a 0 when we hit it
             next_addr = addr - 4
@@ -43,20 +38,25 @@ class DictEntryDumpCmd(gdb.Command):
             addr = next_addr
         raise ValueError("Could not determine start of dictionary entry")
 
-    def _dict_entry_flag_byte(self, addr):
-        b = INFERIOR.read_memory(addr + 4, 1)[0]
+    def _to_cfa(self, dfa):
+        """Convert a dictionary field address to a code field address"""
+        name_size = self._dict_entry_size(dfa)
+        return align(dfa + name_size)
+
+    def _dict_entry_flag_byte(self, dfa):
+        b = INFERIOR.read_memory(dfa + 4, 1)[0]
         b = int.from_bytes(b, byteorder="little") 
         return b
 
-    def _dict_entry_size(self, addr):
-        return self._dict_entry_flag_byte(addr) & F_LENMASK
+    def _dict_entry_size(self, dfa):
+        return self._dict_entry_flag_byte(dfa) & F_LENMASK
 
-    def _dict_entry_name(self, addr, size):
-        name_mem = INFERIOR.read_memory(addr + 5, size)
+    def _dict_entry_name(self, dfa, size):
+        name_mem = INFERIOR.read_memory(dfa + 5, size)
         return name_mem.tobytes().decode("ascii")
 
-    def _dict_entry_flags(self, addr):
-        flag_byte = self._dict_entry_flag_byte(addr)
+    def _dict_entry_flags(self, dfa):
+        flag_byte = self._dict_entry_flag_byte(dfa)
         flags = []
         if flag_byte & F_IMMED:
             flags.append("IMMEDIATE")
@@ -68,10 +68,10 @@ class DictEntryDumpCmd(gdb.Command):
         else:
             return "(None)"
 
-    def _dict_entry_prev(self, addr):
-        link_field = INFERIOR.read_memory(addr, 4).tobytes()
+    def _dict_entry_prev(self, dfa):
+        link_field = INFERIOR.read_memory(dfa, 4).tobytes()
         link_addr = int.from_bytes(link_field, byteorder="little") 
-        return resolve_symbol(link_addr)
+        return self._resolve_symbol(link_addr)
 
     def _dict_entry_body(self, cfa):
         addr = cfa
@@ -81,11 +81,23 @@ class DictEntryDumpCmd(gdb.Command):
             addr += 4
             word = int.from_bytes(mem, byteorder="little") 
             if word:
-                sym = resolve_symbol(word)
+                sym = self._resolve_symbol(word)
                 words.append(f" - {sym}")
             else:
                 break
         return "\n".join(words)
+
+    def _resolve_symbol(self, addr):
+        """Resolve the name of the symbol at ADDR"""
+        sym = gdb.execute(f"info symbol 0x{addr:x}", False, True).strip()
+        if "No symbol matches" in sym:
+            try:
+                dfa = self._to_dfa(addr)
+                size = self._dict_entry_size(dfa)
+                sym = self._dict_entry_name(dfa, size)
+            except:
+                return f"<0x{addr:x}>"
+        return sym
 
     def complete(self, text, word):
         # We expect the argument passed to be a symbol so fallback to the
@@ -93,29 +105,30 @@ class DictEntryDumpCmd(gdb.Command):
         return gdb.COMPLETE_SYMBOL
 
     def invoke(self, args, from_tty):
-        cfa = gdb.parse_and_eval(args)
+        addr = gdb.parse_and_eval(args)
 
         types = {"int", "long", "char *", "void *"}
-        if str(cfa.type) not in types:
-            print(f"Expected address argument of type {types}, got", str(cfa.type))
+        if str(addr.type) not in types:
+            print(f"Expected address argument of type {types}, got", str(addr.type))
             return
 
+        dfa = self._to_dfa(addr)
+        print("Dictionary Field Address:", dfa.format_string(format="x"))
+
+        cfa = self._to_cfa(dfa)
         print("Code Field Address:", cfa.format_string(format="x"))
 
-        addr = self._cfa_to_dfa(cfa)
-        print("Dictionary Field Address:", addr.format_string(format="x"))
-
-        size = self._dict_entry_size(addr)
-        name = self._dict_entry_name(addr, size)
+        size = self._dict_entry_size(dfa)
+        name = self._dict_entry_name(dfa, size)
         print(f"Name:  ({size}) {name}")
 
-        flags = self._dict_entry_flags(addr)
+        flags = self._dict_entry_flags(dfa)
         print(f"Flags: {flags}")
 
         body = self._dict_entry_body(cfa)
         print(f"Body:\n{body}")
 
-        prev = self._dict_entry_prev(addr)
+        prev = self._dict_entry_prev(dfa)
         print(f"Prev: {prev}")
 
 DictEntryDumpCmd()
